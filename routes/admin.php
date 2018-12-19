da51e235a3a5903e5bac0b223d8a965d004225e5
zhang <18238205232@163.com>
2018-12-19 15:20:25 +0800

    cui 12-19 添加路由

--- routes/admin.php
+++ routes/admin.php
@@ -11,6 +11,14 @@ Route::namespace('Admin')->group(function(){
     Route::middleware(['admin_refresh','log'])->group(function(){
         Route::post('user/user_info','UserController@userInfo');
         Route::put('resource/upload','ResourcesController@upload');
+
+        //角色操作
+        Route::get('role/index','ResourcesController@index');    //列表
+        Route::post('role/add','ResourcesController@add');       //添加角色
+        Route::put('role/upload','ResourcesController@store');   //修改角色
+        Route::delete('role/delete','ResourcesController@store');//删除角色
+
+        //权限操作
     });
 });
 
